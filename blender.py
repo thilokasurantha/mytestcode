0696a4e3f16de1266420a48b20fe5446298b4106
Thiloka <thilokasurantha91@gmail.com>
2021-01-14 10:42:07 +0530

    all

--- blender.py
+++ blender.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
 
+root = Tk()
+root.title("Login To The Software")
+root.iconbitmap('favicon.ico')
+
 
 def OK():
     uname = e1.get()
@@ -10,26 +14,50 @@ def OK():
         messagebox.showinfo("" ,"INCORRECT")
     elif (uname == "thiloka" and password == "thiloka123"):
         messagebox.showinfo("", "CORRECT")
+
         root = Tk()
-        
-        def add():
-        
-            e3 = Entry(root)
-            e3.grid(row=2 , column=3)
-            
-        add = Button(root , text="ADD" , command=add , font=("Bold" , 10))
-        add.grid(row=1 , column=1)
+        root.geometry("1350x700+0+0")
+        root.title("Billing Software")
+        root.iconbitmap("favicon (1).ico")
 
+        Label_1 = Label(root, text="Billing Software" , font=("Berlin Sans FB Demi" , 26))
+        Label_1.pack()
 
-        
+        Label_frame = LabelFrame(root, text="Costomer Details" , font=("Berlin Sans FB Demi" , 14))
+        Label_frame.pack(expand='yes' , fill = 'both')
+
+        cn = Label(Label_frame, text="Costomer Name :" ,  font=("Berlin Sans FB Demi" , 15))
+        cn.grid(row=1 , column=1, padx=10)
+
+        cnb = Entry(Label_frame )
+        cnb.grid(row=1 , column=2, padx=10)
+
+        cnom = Label(Label_frame,text="Costomer Number :" ,  font=("Berlin Sans FB Demi" , 15))
+        cnom.grid(row=1 , column=5, padx=10)
+
+        cnb = Entry(Label_frame )
+        cnb.grid(row=1 , column=6, padx=10)
+
+        bn = Label(Label_frame,text="Bill Number :" ,  font=("Berlin Sans FB Demi" , 15))
+        bn.grid(row=1 , column=7, padx=10)
+
+        cnb = Entry(Label_frame )
+        cnb.grid(row=1 , column=8 , padx=10)
+
+        btn1 = Button(Label_frame, text="Search" ,width=8 , height=1)
+        btn1.grid(row=1 , column=17,padx=10)
+
+        Label_frame = LabelFrame(root)
+        Label_frame.pack(expand='yes' , fill = 'both')
+
         root.mainloop()
+
+
     else :
          messagebox.showinfo("", "INCORRECT USERNAME OR PASSWORD")
 
 
-root = Tk()
-root.title("Login To The Software")
-root.iconbitmap('favicon.ico')
+
 
 global e1
 global e2
