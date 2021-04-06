cf610e60be8a45a9a6ab6835716cd99ceabce1a4
Thiloka <thilokasurantha91@gmail.com>
2021-04-06 18:14:59 +0530

    thiloka

--- testing/testing1.py
+++ testing/testing1.py
@@ -1,4 +1,4 @@
-def draw_box(width , leangth):
+def draw_msg_box(width , leangth , msg):
     if (leangth > 50) or (width > 20):
         print("wrong value !")
     else :
@@ -6,10 +6,11 @@ def draw_box(width , leangth):
         find_spaces = leangth - 2
         y = find_spaces
         for x in "#" * width:
-            print(x," " * find_spaces,x)
+            print(x+" " * find_spaces+x)
         print("#" * leangth)
 
 if __name__ == "__main__":
     a = int(input("Enter Your Item Width :"))
     b = int(input("Enter Your Item Leangth :"))
-draw_box(a , b)
+    n = input("Enter Name :")
+draw_msg_box(a , b , n)
