3a213d1e2ec8d9a52ac0009ae300a613c20ac099
Thiloka Surantha <thilokasurantha91@gmail.com>
2021-07-27 19:15:31 +0530

    my_first_class_programme.py

---
+++ Classes/name_age.py
@@ -0,0 +1,8 @@
+class loops :
+    def __init__(self):
+        self.number = 0
+        while(self.number < 100) :
+            self.number += 1
+            print(self.number)
+x = loops()
+print(x)
